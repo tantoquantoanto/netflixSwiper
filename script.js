81603a8cb316b79f630635050b3b4aa8aca3eafa
tantoquantoanto <168585464+tantoquantoanto@users.noreply.github.com>
2024-07-15 02:10:33 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const swiper = new Swiper(".swiper", {
-    direction: "horizontal",
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    slidesPerView: "auto",
-    spaceBetween: 10,
-    breakPoints: {
-        320: {
-            slidesPerView: 1
-        },
-        480: {
-            slidesPerView: 3
-        },
-        640: {
-            slidesPerView: "auto"
-        }
+  direction: "horizontal",
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+  slidesPerView: "auto",
+  spaceBetween: 10,
+  breakPoints: {
+      320: {
+          slidesPerView: 1
+      },
+      480: {
+          slidesPerView: 3
+      },
+      640: {
+          slidesPerView: "auto"
+      }
 
-    }
+  }
 })
 
 
@@ -27,49 +27,49 @@ const anchorInput = document.querySelector("#inputContainer a");
 console.log(anchorInput);
 
 const createInput = () => {
-    let divInput = inputContainer.querySelector('input');
-    if (!divInput) {
-        divInput = document.createElement("input");
-        divInput.setAttribute("type", "text");
-        inputContainer.insertBefore(divInput, anchorInput);
-    }
-    return divInput;
+  let divInput = inputContainer.querySelector('input');
+  if (!divInput) {
+      divInput = document.createElement("input");
+      divInput.setAttribute("type", "text");
+      inputContainer.insertBefore(divInput, anchorInput);
+  }
+  return divInput;
 };
 
 anchorInput.addEventListener("click", (e) => {
-   
-    const inputElement = createInput();
-    
-    if (inputElement.style.display === 'none' || inputElement.style.display === '') {
-        inputElement.style.display = 'block';
-    } else {
-        inputElement.style.display = 'none';
-    }
+ 
+  const inputElement = createInput();
+  
+  if (inputElement.style.display === 'none' || inputElement.style.display === '') {
+      inputElement.style.display = 'block';
+  } else {
+      inputElement.style.display = 'none';
+  }
 });
 
 
 
-const elementToTrack = document.querySelectorAll('.sectionToShow')
-console.log(elementToTrack)
+const elementsToShow = document.querySelectorAll('.sectionToShow')
+
 
 const options = {
-    rootMargin: '0px',
-    threshold: 0.8 // 50%
+  rootMargin: '0px',
+  threshold: 0.8 // 50%
 }
 
-const callBack = (entries, observer) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('visible')
-        } else {
-            entry.target.classList.remove('visible')
-        }
-    })
+const showSection = (sections, observer) => {
+  sections.forEach(section => {
+      if (section.isIntersecting) {
+          section.target.classList.add('visible')
+      } else {
+          section.target.classList.remove('visible')
+      }
+  })
 }
 
-const observer = new IntersectionObserver(callBack, options)
+const observer = new IntersectionObserver(showSection, options)
 
 
-elementToTrack.forEach(element => {
-    observer.observe(element)
+elementsToShow.forEach(element => {
+  observer.observe(element)
 })
